b30b392e7547371d74ab8c00b6da53a7b093f37d
Rohit Mundayaliyath Mundayadan <rmundaya@purdue.edu>
2015-12-01 17:58:15 -0500

    Changes

--- MapReduceDental/src/main/java/group05/bigdata/driver/StateTaxDriver.java
+++ MapReduceDental/src/main/java/group05/bigdata/driver/StateTaxDriver.java
@@ -1,5 +1,20 @@
 package group05.bigdata.driver;
 
-public class StateTaxDriver {
+import org.apache.hadoop.util.ToolRunner;
 
+public class StateTaxDriver extends DiverBase{
+
+	public static void main( String[] args ) throws Exception
+    {
+    	int code = ToolRunner.run(new StateTaxDriver(), args);
+        System.exit(code);
+    }
+	StateTaxDriver(){
+		job.setJobName("StateTaxJob");
+		//Set the mapper class
+		job.setMapperClass(group05.bigdata.mappers.StateTaxMapper.class);
+		job.setReducerClass(group05.bigdata.reducer.StateTaxReducer.class);
+		// Set the output path of file. Timestamp embedding to prevent write failures on HDFS
+		outputFileName = "StateTax";
+	}
 }
--- MapReduceDental/src/main/java/group05/bigdata/mappers/StateTaxMapper.java
+++ MapReduceDental/src/main/java/group05/bigdata/mappers/StateTaxMapper.java
@@ -1,5 +1,33 @@
 package group05.bigdata.mappers;
 
-public class StateTaxMapper {
+import java.io.IOException;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
+public class StateTaxMapper  extends  Mapper<LongWritable, Text, Text, FloatWritable> {
+	public void map(LongWritable lineOffSet, Text record, Context context)
+			throws IOException, InterruptedException {
+		
+		String[] columnValues = record.toString().split(",");
+		/*
+		 * [0] State
+		 * [1] Base Tax
+		 * [2] Surplus Tax	
+		 */
+		if(columnValues.length != 3){
+			//Check for bad row
+			return;
+        }	
+		String stateKey = columnValues[0].trim();      
+	    
+		try{
+			float taxValue = Float.parseFloat(columnValues[1].trim());
+			context.write(new Text(stateKey), new FloatWritable(taxValue));
+		}
+		catch(NumberFormatException e){
+			return;
+		}  
+	}
 }
